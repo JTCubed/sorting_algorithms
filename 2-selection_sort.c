fbc1dcd567b5179a7a0493086768a0ba0328ece7
JTCubed <zeusio60@gmail.com>
2023-11-21 01:21:21 -0800

    hot fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
-
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
